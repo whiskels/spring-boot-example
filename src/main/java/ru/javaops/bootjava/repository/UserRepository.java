9e00bb549c87bfba888ca52c9bad7d88632efe26
whiskels <kuzmingeo@gmail.com>
2020-12-02 17:06:26 +0300

    update: add data jpa

---
+++ src/main/java/ru/javaops/bootjava/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package ru.javaops.bootjava.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javaops.bootjava.model.User;
+
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @RestResource(rel = "by-email", path = "by-email")
+    @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    @RestResource(rel = "by-lastname", path = "by-lastname")
+    Page<User> findByLastNameContainingIgnoreCase(String lastName, Pageable page);
+}
